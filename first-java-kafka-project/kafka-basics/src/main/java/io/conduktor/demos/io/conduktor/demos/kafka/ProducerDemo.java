842798104a3097141f1e05cacc36e314f4e1d2da
48308-Nuno Neves <a48308@alunos.isel.pt>
2025-08-08 12:15:22 +0100

    Add kafka demos

---
+++ first-java-kafka-project/kafka-basics/src/main/java/io/conduktor/demos/io/conduktor/demos/kafka/ProducerDemo.java
@@ -0,0 +1,38 @@
+package io.conduktor.demos.io.conduktor.demos.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("I am kafka producer");
+
+        // Create producer properties
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers","127.0.0.1:19092");
+
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer",StringSerializer.class.getName());
+
+        // Create the Producer
+        KafkaProducer<String,String> producer = new KafkaProducer<>(properties);
+
+        // Create the Producer Record
+        ProducerRecord<String,String> producerRecord = new ProducerRecord<>("demo_java", "Hello World");
+
+        // Send data
+        producer.send(producerRecord);
+
+        // Flush and close the producer
+        producer.flush();
+        producer.close();   // Also runs the flush, the flush above is not needed
+    }
+}
